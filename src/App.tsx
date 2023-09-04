ab9c9d5e75b88ba47fab3e535f1e81f678010bc1
Pavel Belyakov <theflashofsonic@gmail.com>
2023-09-04 07:25:57 +0100

    onesignal

--- src/App.tsx
+++ src/App.tsx
@@ -2,53 +2,56 @@ import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import OneSignal from 'react-onesignal';
 
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-import { getMessaging, getToken, onMessage } from 'firebase/messaging';
+// import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+// import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyDmu8NQ1kfFCvYDabdxQkiExo02ICx7KNM',
-  authDomain: 'easy-a9ed2.firebaseapp.com',
-  projectId: 'easy-a9ed2',
-  storageBucket: 'easy-a9ed2.appspot.com',
-  messagingSenderId: '910347933924',
-  appId: '1:910347933924:web:1b135329711facdf5fb4f1',
-  measurementId: 'G-HFQJT7R2X0',
-};
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyDmu8NQ1kfFCvYDabdxQkiExo02ICx7KNM',
+//   authDomain: 'easy-a9ed2.firebaseapp.com',
+//   projectId: 'easy-a9ed2',
+//   storageBucket: 'easy-a9ed2.appspot.com',
+//   messagingSenderId: '910347933924',
+//   appId: '1:910347933924:web:1b135329711facdf5fb4f1',
+//   measurementId: 'G-HFQJT7R2X0',
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-getAnalytics(app);
-const messaging = getMessaging(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// getAnalytics(app);
+// const messaging = getMessaging(app);
 
-onMessage(messaging, (payload) => {
-  console.log('Message received. ', payload);
-  // if (payload.notification?.title) {
-  //   new Notification(payload.notification?.title, { body: 'Main thread notification' });
-  // }
-  alert(payload.notification?.title);
-});
+// onMessage(messaging, (payload) => {
+//   console.log('Message received. ', payload);
+//   // if (payload.notification?.title) {
+//   //   new Notification(payload.notification?.title, { body: 'Main thread notification' });
+//   // }
+//   alert(payload.notification?.title);
+// });
+
+OneSignal.init({ appId: '484f4a51-583d-459e-a3e4-9148f72e068c' });
 
 function App() {
   const [count, setCount] = useState(0);
-  const [fcmToken, setFcmToken] = useState<string | undefined>();
+  const [fcmToken] = useState<string | undefined>();
 
   const { permissionState, askForPermission } = useNotificationPermission();
 
-  useEffect(() => {
-    (async () => {
-      if (permissionState === 'granted') {
-        const serviceWorkerRegistration = await navigator.serviceWorker.register('/firebase-messaging-sw.js');
-        getToken(messaging, {
-          vapidKey: 'BPDY0gTMm4zHdDd7VG1VeE9-c7aQBZxYZQFxoYX20LNncayOyC0oaYyzN0-bFp2hpTbn-NbDqGHvbP8uDGO5p8c',
-          serviceWorkerRegistration,
-        }).then((token) => {
-          setFcmToken(token);
-        });
-      }
-    })();
-  }, [permissionState]);
+  // useEffect(() => {
+  //   (async () => {
+  //     if (permissionState === 'granted') {
+  //       const serviceWorkerRegistration = await navigator.serviceWorker.register('/firebase-messaging-sw.js');
+  //       getToken(messaging, {
+  //         vapidKey: 'BPDY0gTMm4zHdDd7VG1VeE9-c7aQBZxYZQFxoYX20LNncayOyC0oaYyzN0-bFp2hpTbn-NbDqGHvbP8uDGO5p8c',
+  //         serviceWorkerRegistration,
+  //       }).then((token) => {
+  //         setFcmToken(token);
+  //       });
+  //     }
+  //   })();
+  // }, [permissionState]);
 
   const copyFcm = () => {
     if (!fcmToken) return;
@@ -78,6 +81,7 @@ function App() {
       <button onClick={copyFcm}>Copy FCM</button>
       <button onClick={askForPermission}>Ask for notifications permission</button>
       <button onClick={generateNotification}>Generate notification ( 10 sec timeout )</button>
+      <div className="onesignal-customlink-container"></div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
